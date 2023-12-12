12dbfed83bed583c949a091779953b13122064f1
mdArshad10 <mdarshd246@gmail.com>
2023-12-12 13:20:10 +0530

    adding the email or phone number validation in customerDao.java

--- src/main/java/controller/CustomerSignup.java
+++ src/main/java/controller/CustomerSignup.java
@@ -15,57 +15,49 @@ import dao.CustomerDao;
 import dto.Customer;
 
 @WebServlet("/customerSignup")
-public class CustomerSignup extends HttpServlet{
+public class CustomerSignup extends HttpServlet {
 	@Override
 	protected void service(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
-		  String cusname = req.getParameter("cname");
-		 String mobile =req.getParameter("mobile");
-		 
+		String cusname = req.getParameter("cname");
+		String mobile = req.getParameter("mobile");
+
 //		 mobile number is in long data so convert the string to long by applying parsing concept
-		 long mob = Long.parseLong(mobile);
-		 
-		 String password =req.getParameter("password");
-		 String cus_email =req.getParameter("cus_email");
-		 String gender =req.getParameter("gender");
-		 String dob =req.getParameter("dob");
-		 
-//		 System.out.println("the customer name is "+ cusname);
-//		 System.out.println("the mobile no. is "+ mob);
-//		 System.out.println("the passowrd is "+ password);
-//		 System.out.println("the customer email is "+ cus_email);
-//		 System.out.println("the gender of customer is "+ gender);
-//		 System.out.println("the date of birth is "+ dob);
-//		 
-//		 res.getWriter().print("<h1> "+ cusname + "</h1>"
-//		 		+ "<h1> "+ mob + "</h1>"
-//		 		+ "<h1> "+ password+ "</h1>"
-//		 		+ "<h1> " + cus_email+ "</h1>"
-//		 		+ "<h1> " + gender+ "</h1>"
-//		 		+ "<h1> " + dob+ "</h1>");
-		 
-		 // converting the String datatype to date
-		 Date date = Date.valueOf(dob);
-//		 System.out.print("the date is "+date);
-		 Period period =Period.between(date.toLocalDate(), LocalDate.now());
-//		 System.out.println("the period is "+period);
-		 int age = period.getYears();
-//		 System.out.println("the age is "+age);
-		 Customer customer = new Customer();
-		 CustomerDao customerDao = new CustomerDao();
-		 if(age>30) {
-//			 res.getWriter().print("<h1>eligiblie</h1>");
-			 customer.setCname(cusname);
-			 customer.setEmail(cus_email);
-			 customer.setPassword(password);
-			 customer.setGender(gender);
-			 customer.setDob(date);
-			 customer.setMobile(mob);
-			 
-			 customerDao.save(customer);
-			 res.getWriter().print("<h1>Account created successfully</h1");
-		 }else {
-			 res.getWriter().print("<h1>your are not eligible</h1>");
-		 }
-//		 res.getWriter().print("<h1> the date "+date+" " +" this is period "+ period+"</h1>");
+		long mob = Long.parseLong(mobile);
+
+		String password = req.getParameter("password");
+		String cus_email = req.getParameter("cus_email");
+		String gender = req.getParameter("gender");
+		String dob = req.getParameter("dob");
+
+		// converting the String datatype to date
+		Date date = Date.valueOf(dob); //
+
+		Period period = Period.between(date.toLocalDate(), LocalDate.now()); //
+
+		int age = period.getYears(); //
+
+		Customer customer = new Customer();
+		CustomerDao customerDao = new CustomerDao();
+		// check the age valiadation
+		if (age > 30) {
+			// check the email or phone number validation
+			if (customerDao.check(cus_email, mob).isEmpty()) {
+				// created
+				customer.setCname(cusname);
+				customer.setEmail(cus_email);
+				customer.setPassword(password);
+				customer.setGender(gender);
+				customer.setDob(date);
+				customer.setMobile(mob);
+
+				customerDao.save(customer);
+				res.getWriter().print("<h1>Account created successfully</h1");
+
+			} else {
+				res.getWriter().print("<h1>Credientail are already exist</h1>");
+			}
+		} else {
+			res.getWriter().print("<h1>your are not eligible</h1>");
+		}
 	}
 }
--- src/main/java/dao/CustomerDao.java
+++ src/main/java/dao/CustomerDao.java
@@ -1,9 +1,12 @@
 package dao;
 
+import java.util.List;
+
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import javax.persistence.Query;
 
 import dto.Customer;
 
@@ -11,10 +14,25 @@ public class CustomerDao {
 	EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("dev");
 	EntityManager entityManager = entityManagerFactory.createEntityManager();
 	EntityTransaction entityTransaction = entityManager.getTransaction();
-	
+
 	public void save(Customer customer) {
 		entityTransaction.begin();
 		entityManager.persist(customer);
 		entityTransaction.commit();
+	}
+
+//	for email or phone number validation
+	public List<Customer> check(String email, long mobile) {
+
+		// Method - 1
+		List<Customer> list = entityManager.createQuery("select z from Customer z where email=?1 and mobile=?2")
+				.setParameter(1, email).setParameter(2, mobile).getResultList();
+		// method - 2
+//	Query query = entityManager.createQuery("select z from Customer z where email=?1 and mobile=2?");
+//	query.setParameter(1, email);
+//	 query.setParameter(2, mobile);
+//	List<Customer> list = query.getResultList();
+		return list;
+
 	}
 }
